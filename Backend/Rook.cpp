4740792c44dc270ca9c02c6c23f6700dfee90970
meny1237 <meny1237@donotreply.com>
2015-12-28 19:12:28 +0200

    NewRook

--- Backend/Rook.cpp
+++ Backend/Rook.cpp
@@ -12,7 +12,11 @@ Rook::Rook(Location loc, int player ,bool onBoard) : Tool(loc, player, onBoard)
 char Rook::getSymbol()
 {
 	char ans;
-	if (this->_player == 0)
+	if (this == nullptr)
+	{
+		ans = '#';
+	}
+	else if (this->_player == 0)
 	{
 		ans = 'R';
 	}
@@ -28,6 +32,13 @@ int Rook::moveTool(string command)
 	int dstJ = command[2] - 97;
 	int dstI = command[3] - 48;
 	string dstTile = command[2] + "" + command[3];
+	if (command[0] == command[2] || command[1] == command[3])
+	{
+		if (command[0] == command[2])
+		{
+			while (command[0] - 'a' )
+		}
+	}
 	/*if (this->_board->getTile(dstTile)->isOccupied() == false)
 	{
 		cout << dstI << " ," << dstJ << endl;
