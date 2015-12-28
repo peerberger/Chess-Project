f5901b3fe93d1c51b872cd416d362dc93bc361f7
Yehuda <yehudaac1@gmail.com>
2015-12-28 18:08:25 +0200

    BLA

---
+++ Backend/Tile.cpp
@@ -0,0 +1,58 @@
+/*
+	This header file is a part from Chess project by:
+		Yehuda Chikvashvili
+		Meny Menasherov
+		Pe'er Berger
+	
+	Tile class implementation code.
+*/
+
+#include "Tile.h"
+
+
+Tile::Tile ( Location location, Tool* tool )
+	:_location ( location ), _tool ( tool )
+{
+}
+
+
+Tile::~Tile ()
+{
+	this->_location = "";
+	this->setTool ( nullptr );
+}
+
+
+bool Tile::isOccupied () const
+{
+	return this->_tool != nullptr;
+}
+
+
+Location Tile::getLocation () const
+{
+	return this->_location;
+}
+
+
+Tool* Tile::getTool () const
+{
+	return this->_tool;
+}
+
+
+void Tile::setLocation ( Location location )
+{
+	this->_location = location;
+}
+
+
+void Tile::setTool ( Tool* tool )
+{
+	if ( this->_tool != nullptr )
+	{
+		delete this->_tool;
+	}
+
+	this->_tool = tool;
+}
