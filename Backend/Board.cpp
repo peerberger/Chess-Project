a10c57a59fcaef34b1f908688b48ecd5a54c6762
Yehuda <yehudaac1@gmail.com>
2015-12-28 18:33:11 +0200

    bla

---
+++ Backend/Board.cpp
@@ -0,0 +1,18 @@
+/*
+	This header file is a part from Chess project by:
+		Yehuda Chikvashvili
+		Meny Menasherov
+		Pe'er Berger
+	
+	Tile class implementation code.
+*/
+
+#include "Board.h"
+#include "Rook.h"
+
+
+Board::Board ( int starting_player )
+	:_currPlayer ( starting_player )
+{
+	
+}
