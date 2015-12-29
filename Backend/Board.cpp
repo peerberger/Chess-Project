23ea94f94b8af0beb8cf63be5746adecb95a54c3
Yehuda <yehudaac1@gmail.com>
2015-12-29 16:48:54 +0200

    Rook Demo: ALMOST THERE!!

--- Backend/Board.cpp
+++ Backend/Board.cpp
@@ -10,6 +10,12 @@
 #include "Board.h"
 #include "Rook.h"
 
+// Side functions
+bool commandLengthCheck ( string move );
+bool tileIndexCheck ( string tile );
+bool toolOwnerCheck ( char symbol, int player );
+
+
 
 Board::Board ( int starting_player )
 	:_currPlayer ( starting_player )
@@ -75,67 +81,84 @@ Tile* Board::getTile ( string tile ) const
 *************************/
 int Board::play ( string move )
 {
-	int answer = 0;
-	if ( move.length () != 4 )
+	if ( commandLengthCheck ( move ) )
 	{
-		answer = 5;
-	}
-	else
-	{
-		string source = move.substr ( 0, 2 );
-		string destination = move.substr ( 2 );
-
-		if ( source == destination )
-		{
-			answer = 7;
-		}
-		else
-		{
-			Tile* srcTile = this->getTile ( source );
-			Tile* dstTile = this->getTile ( destination );
-
-			if	(
-					!( 
-						srcTile->getTool ()->getSymbol () > 'a' - this->_currPlayer*( 'a' - 'A' )
-						&&
-						srcTile->getTool ()->getSymbol () < 'z' - this->_currPlayer * ( 'a' - 'A' )
-					) 
-				)
-			{
-				answer = 2;
-			}
-			else if (
-					dstTile->getTool ()->getSymbol () > 'a' - this->_currPlayer*( 'a' - 'A' )
-					&&
-					dstTile->getTool ()->getSymbol () < 'z' - this->_currPlayer * ( 'a' - 'A' )
-					)
-			{
-				answer = 3;
-			}
-			else if ( 0 /* Check if the owner king is threatened */ )
-			{
-				answer = 4;
-			}
-			else
-			{
-				answer = srcTile->getTool ()->moveTool ( destination );
-
-				if ( answer == 0 )
-				{
-					if ( 0 /* Check if the rival king is threatened */ )
-					{
-						answer = 1;
-
-						if ( 0 /* Check if the rival king can't move*/ )
-						{
-							answer = 8;
-						}
-
-					}
-				}
-			}
-		}
+		return 5; // Invalid tiles indexes -> the input string length is invalid
 	}
 
+	string src = move.substr ( 0, 2 );
+	string dst = move.substr ( 2 );
+	
+	if ( tileIndexCheck ( src ) || tileIndexCheck ( dst ) )
+	{
+		return 5;
+	}
+
+	if ( src == dst )
+	{
+		return 7; //source tile and destination tile are equal
+	}
+
+	Tile* from = this->getTile ( src );
+	Tile* to = this->getTile ( dst );
+	
+	if ( toolOwnerCheck ( from->getTool ()->getSymbol () , this->_currPlayer ))
+	{
+		return 2; // Source tile has no tool which belongs to the current player
+	}
+
+	if ( !toolOwnerCheck ( to->getTool ()->getSymbol (), this->_currPlayer ) )
+	{
+		return 3;
+	}
+
+	if ( 0 /* Check if the owner king is threatened */ )
+	{
+		return 4;
+	}
+
+	int answer = from->getTool ()->moveTool ( dst );
+
+	if ( answer == 0 )
+	{
+		if ( 0 /* Check if the rival king is threatened */ )
+		{
+			answer = 1;
+
+			if ( 0 /* Check if the rival king can't move*/ )
+			{
+				answer = 8;
+			}
+
+		}
+
+		to->setTool ( from->getTool () );
+		from->setTool ( nullptr );
+		this->_currPlayer = !this->_currPlayer; // Change the current player
+	}
+	
 	return answer;
+}
+
+
+bool commandLengthCheck ( string move )
+{
+	return move.length() != 4;
+}
+
+
+bool tileIndexCheck ( string tile )
+{
+	return ( tile [ 0 ] < 'a' || tile [ 0 ] > 'z' || tile [ 1 ] <'0' || tile [ 1 ] > '9' );
+}
+
+
+bool toolOwnerCheck ( char symbol, int player )
+{
+	return
+		(
+		symbol < 'a' - player*( 'a' - 'A' )
+		||
+		symbol > 'z' - player*( 'z' - 'Z' )
+		);
 }
