a9adf4ba2b8d8dc011458c0ba6704aa5741d7077
meny1237 <meny1237@donotreply.com>
2015-12-30 17:47:44 +0200

    FINAL KING

--- Backend/King.cpp
+++ Backend/King.cpp
@@ -16,7 +16,20 @@ char King::getSymbol()
 	return symbol;
 }
 
-int King::moveTool(string move)
+int King::moveTool(string command)
 {
-
+	int valid = 0;
+	int srcX = command[0] - 'a';
+	int srcY = command[1] - '1';
+	int dstX = command[2] - 'a';
+	int dstY = command[3] - '1';
+	if (!((abs(srcX - dstX) == 1) ^ (abs(srcY - dstY) == 1))) // Check a rook movement for only 1 tile.
+	{
+		valid = 6;
+	}
+	else if (abs(srcX - dstX) != abs(srcY - dstY) || abs(srcX-dstX)>1 || abs(srcY-dstY)>1)// Check a bishop movement for only one tile
+	{
+		valid = 6;
+	}
+	return valid;
 }
